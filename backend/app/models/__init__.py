124a749f52488a9d3a99db02645374bfe4e17bc2
SuneethaNaidu <suneethanaidu137@gmail.com>
2026-07-12 12:10:35 +0530

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.role import Role
+from app.models.department import Department
+from app.models.user import User
+from app.models.asset_category import AssetCategory
+from app.models.asset import Asset
+from app.models.asset_allocation import AssetAllocation
